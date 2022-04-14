a0cb8399ec9d42d4427cd79b81855c7efef1b047
mohammed thair <mohammedthair@mohammeds-MacBook-Air.local>
2022-04-14 23:02:24 +0300

    update

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = 'wiki'
 
-urlpatterns = {
+urlpatterns = [
     path('wiki/create/', views.create_entry, name='create_entry'),
     path('wiki/edit/<str:title>/', views.edit_entry, name='edit_entry'),
     path('wiki/<str:title>', views.single_entery, name='single-entry'),
     path('wiki/random', views.random_entry, name='random'),
 
     path("", views.index, name="index"),
-}
+]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 import random
-import content as content
+
 from django.shortcuts import redirect
 from django.shortcuts import render
 
@@ -11,21 +11,19 @@ def index(request):
     q = request.GET.get('q')
     entries = util.list_entries()
     if q:
-        entries=[e for e in entries if q.lower() in e.lower()]
+        entries = [e for e in entries if q.lower() in e.lower()]
     if q in entries:
-            return redirect('wiki:single-entry', q)
-    return  render(request, "encyclopedia/index.html", {
+        return redirect('wiki:single-entry', q)
+    return render(request, "encyclopedia/index.html", {
         "entries": entries
     })
-
-
 
 
 def single_entery(request, title: str):
     content = util.get_entry(title)
     if not content:
-        return render(request,'encyclopedia/error.html',{
-            title:'404'
+        return render(request, 'encyclopedia/error.html', {
+            'title': '404'
         })
     return render(request, 'encyclopedia/single_entry.html', {
         'title': title,
@@ -43,14 +41,12 @@ def create_entry(request):
     if request.method == 'POST':
         form = EntryCreateForm(request.POST)
         if form.is_valid():
-            title = form.cleaned_data['title'], form.cleaned_data['content']
+            title, content = form.cleaned_data['title'], form.cleaned_data['content']
             if title in util.list_entries():
-                return render( request , 'encyclopedia/error.html',{
+                return render(request, 'encyclopedia/error.html', {
                     'title': f'The entry {title} already exists'
                 })
             util.save_entry(title, content)
-
-
 
             return redirect('wiki:index')
 
@@ -61,7 +57,9 @@ def create_entry(request):
 
 def edit_entry(request, title: str):
     return None
-def random_entry (request):
-    entries=util.list_entries()
+
+
+def random_entry(request):
+    entries = util.list_entries()
     random_choice = random.choice(entries)
     return redirect('wiki:single-entry', random_choice)
